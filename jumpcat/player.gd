c343093b6101776a6a4acb66108d2db3d56a5e2a
Stefan <hoermann@cpg-gilching.de>
2024-11-06 16:57:54 +0100

    Stand 06.11.2024
    Started Jumpcat
    Added Player
    Added Level

---
+++ jumpcat/player.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+
+
+const JUMP_VELOCITY = -700.0
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		$AnimatedSprite2D.play("jump")
+		velocity += get_gravity() * delta
+	else:
+		$AnimatedSprite2D.play("walk")
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+
+	move_and_slide()
