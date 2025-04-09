3e41a8c2c66fb5457beb5186c4fcddd7fe4f3335
Stefan <hoermann@cpg-gilching.de>
2025-04-09 16:58:03 +0200

    Stand 09.04.
    Schöne Osterferien euch allen!!!

--- spookybricks/Levels/bricks.gd
+++ spookybricks/Levels/bricks.gd
@@ -1,5 +1,13 @@
 extends TileMapLayer
 
+@export_range(0,1) var powerup_range = 0.5
+
+const UNBREAKABLE_BRICK = Vector2i(1,2)
+const STRONG_BRICK = Vector2i(2,1)
+const CRACKED_BRICK = Vector2i(0,2)
+
+signal spawn_powerup
+
 func brick_hit(pos, normal):
 	var brick_pos = local_to_map(pos-normal)
 	check_brick_type(brick_pos)
@@ -8,12 +16,23 @@ func brick_hit(pos, normal):
 
 
 func check_brick_type(brick_pos):
-	# TODO: nächste Stunde checken wir den Typ des Bricks
-	if not get_cell_atlas_coords(brick_pos) == Vector2i(1,2):
+	if get_cell_atlas_coords(brick_pos) == UNBREAKABLE_BRICK:
+		return
+	elif get_cell_atlas_coords(brick_pos) == STRONG_BRICK:
+		# Strong Brick in Cracked Brick umwandeln
+		set_cell(brick_pos, 1, CRACKED_BRICK)
+	else:
 		erase_cell(brick_pos)
+		check_powerup(map_to_local(brick_pos))
+
+
+func check_powerup(brick_pos):
+	if randf() <= powerup_range:
+		emit_signal("spawn_powerup", brick_pos)
 
 
 func no_bricks_remain():
 	var blocks_left = get_used_cells().size()
-	if blocks_left == 0:
+	var unbreakable_blocks_left = get_used_cells_by_id(1, UNBREAKABLE_BRICK).size()
+	if blocks_left == unbreakable_blocks_left:
 		return true
---
+++ spookybricks/Levels/powerup_controller.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+
+func _on_bricks_spawn_powerup(brick_pos) -> void:
+	print("powerup spawned at: " + str(brick_pos))
+
+# Weiter geht's nach den Osterferien!!!!!
